d5849b4bea937ed7cdbcd37a6fb181531b9efeca
Michal <mp.malyszek@gmail.com>
2019-01-07 22:42:22 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,102 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    constructor() {
+        super();
+        this.state = {
+            seat: [
+                'A1', 'A', 'A3',
+                'B1', 'B2', 'B3',
+                'C1', 'C2', 'C3'
+            ],
+            seatAvailable: [
+                'A1', 'A2', 'A3',
+                'B1', 'B2', 'B3',
+                'C1', 'C2', 'C3'
+            ],
+            seatReserved: []
+        }
+    }
+
+    onClickData(seat) {
+        if (this.state.seatReserved.indexOf(seat) > -1) {
+            this.setState({
+                seatAvailable: this.state.seatAvailable.concat(seat),
+                seatReserved: this.state.seatReserved.filter(res => res != seat)
+            })
+        } else {
+            this.setState({
+                seatReserved: this.state.seatReserved.concat(seat),
+                seatAvailable: this.state.seatAvailable.filter(res => res != seat)
+            })
+        }
+    }
+
+    render() {
+        return ( 
+            <div>
+            <h1> Seat Reservation System </h1>  
+            <DrawGrid 
+            seat = {this.state.seat}
+            available = {this.state.seatAvailable}
+            reserved = {this.state.seatReserved}
+            onClickData = {this.onClickData.bind(this)}/>  
+            </div>
+        )
+    }
+}
+
+class DrawGrid extends React.Component {
+        render() {
+                return ( 
+                    <div className = "container" >
+                    <h2> </h2>  
+                    <table className = "grid" >
+                    <tbody >
+                    <tr> {this.props.seat.map(row =>
+                            <td className = {this.props.reserved.indexOf(row) > -1 ? 'reserved' : 'available'}
+                            key = {row}
+                            onClick = {e => this.onClickSeat(row)} > {row} </td>) }  
+                            </tr>  
+                            </tbody>  
+                            </table>
+
+                            <AvailableList available = {this.props.available}/>  
+                            <ReservedList reserved = {this.props.reserved}/>  
+                    /div>
+                )
+        }
+
+                    onClickSeat(seat) {this.props.onClickData(seat);
+    }
+}
+
+                class AvailableList extends React.Component {
+                        render() {
+                                const seatCount = this.props.available.length;
+                                return ( <div className = "left" >
+                                    <h4> Available Seats: ({
+                                        seatCount == 0 ? 'No seats available' : seatCount}) </h4>  
+                                        <ul > {this.props.available.map(res => <li key = {res}> {res} </li> )}  
+                                        </ul>  
+                                        </div>
+                                )
+                        }
+                }
+
+class ReservedList extends React.Component {
+    render() {
+         return ( 
+              <div className = "right" >
+              <h4> Reserved Seats: ({this.props.reserved.length}) </h4>  
+              <ul> {this.props.reserved.map(res => < li key = {res} > {res} </li>) }  
+              </ul> 
+              </div>
+            )
+     }
+}
+
+
+ReactDOM.render( < App name = "Rachelle" / > , document.getElementById('app'));
